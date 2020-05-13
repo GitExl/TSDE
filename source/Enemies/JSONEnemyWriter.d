00cb22b6b9cf994ee55dc99fad4366086f637859
Dennis Meuwissen <GitExl@users.noreply.github.com>
2020-05-13 21:25:23 +0200

    Apply license.

--- source/Enemies/JSONEnemyWriter.d
+++ source/Enemies/JSONEnemyWriter.d
@@ -1,3 +1,11 @@
+/**
+ * Time Switch Data Extractor version 1.0  Copyright (C) 2020
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it under
+ * certain conditions, see the accompanying COPYING file.
+ */
+
 module Enemies.JSONEnemyWriter;
 
 import std.string;
--- source/app.d
+++ source/app.d
@@ -1,3 +1,11 @@
+/**
+ * Time Switch Data Extractor version 1.0  Copyright (C) 2020
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it under
+ * certain conditions, see the accompanying COPYING file.
+ */
+
 import std.stdio;
 import std.json;
 
@@ -7,7 +15,8 @@ import BinaryFile;
 import JSONTools;
 
 int main(string[] argv) {
-    writeln("Time Switch Data Extractor version 1.0");
+    writeln("Time Switch Data Extractor version 1.0  Copyright (C) 2020");
+    writeln("This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions, see the accompanying COPYING file.");
     writeln();
 
     if (argv.length != 4) {
